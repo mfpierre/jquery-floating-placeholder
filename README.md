c5c6fac7265759280d659618559e3e61c5f7f8f7
Pierre <mfpierre@gmail.com>
2013-10-15 01:16:02 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-jquery-floating-placeholder
-===========================
+# jquery-floating-placeholder
 
 A very simple and light weight jQuery plugin that makes the placeholder float above the input when typing
+
+Based on a concept found on Dribble:
+http://dribbble.com/shots/1254439--GIF-Mobile-Form-Interaction?list=users
+
+![Dribble GIF](http://dribbble.s3.amazonaws.com/users/6410/screenshots/1254439/form-animation-_gif_.gif)
+
+## Options
+*   placeholderActiveColor
+*   placeholderIdleColor
+*   activeClass
+*   helperClass
+*   transitionTime
---
+++ jquery-floating-placeholder.js
@@ -0,0 +1,56 @@
+/* ===========================================================
+ * jquery-floating-placeholder.js v1
+ * ===========================================================
+ * Copyright 2013 Pierre Margueritte
+ * http://www.pinxit.me
+ *
+ * A very simple and light weight jQuery plugin that
+ * makes the placeholder float above the input when typing
+ *
+ * https://github.com/peachananr/simple-text-rotator
+ *
+ * ========================================================== */
+(function ($) {
+
+    $.fn.floatingPlaceholder = function (options) {
+        var settings = $.extend({
+            placeholderActiveColor: '#0079FF',
+            placeholderIdleColor: '#BBBBBB',
+            activeClass: 'placeholder-floating',
+            helperClass: 'placeholder-helper',
+            transitionTime: '200'
+        }, options);
+
+        var wrapper = $('<div>').css({
+            'position': 'relative'
+        });
+        $(this).wrap(wrapper);
+        $(this).on('keyup', function () {
+            if ($(this).val()) {
+                if (!$(this).hasClass(settings.activeClass)) {
+                    var placeholderHelper = $("<span>").addClass(settings.helperClass).css({
+                        'position': 'absolute',
+                        'color': settings.placeholderActiveColor
+                    });
+                    $(this).parent().prepend($(placeholderHelper).html($(this).attr('placeholder')).hide().fadeIn(settings.transitionTime));
+                    $(this).addClass(settings.activeClass).animate({
+                        marginTop: '+=' + $(this).prev('span').outerHeight(true) + 'px'
+                    }, settings.transitionTime);
+                }
+            } else if ($(this).hasClass(settings.activeClass)) {
+                $(this).prev('span').fadeOut(settings.transitionTime, function () {
+                    $(this).next('input').animate({
+                        marginTop: '-=' + $(this).outerHeight(true) + 'px'
+                    }, settings.transitionTime).removeClass(settings.activeClass);
+                    $(this).remove();
+                });
+            }
+        }).on('focus', function () {
+            $(this).prev('span').css('color', settings.placeholderActiveColor);
+        })
+            .on('focusout', function () {
+                $(this).prev('span').css('color', settings.placeholderIdleColor);
+            });
+    };
+
+}(jQuery));
